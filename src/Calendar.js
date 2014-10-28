062ed1c2b34cfe7fea576c9be326214092293334
Jonathan Wheeler <jonny@njtechnologies.co.uk>
2014-10-28 10:56:44 +0000

    option to not re-render views on resource changes

--- src/Calendar.js
+++ src/Calendar.js
@@ -582,7 +582,7 @@ function Calendar(element, instanceOptions) {
 		renderEvents();
 	}
 
-	function addEventResource(resource) {
+	function addEventResource(resource, reRenderView) {
 		if(!resourcesByID(resource.id)) {
 			eventResources.push(resource);
 		}
@@ -590,12 +590,13 @@ function Calendar(element, instanceOptions) {
 			associateResourceWithEvent(events[i]);
 		}
 		orderResources();
-		renderView(null, currentView.name, true);
+		if (reRenderView) {renderView(null, currentView.name, true);}
 	}
 
 		
-	function removeEventResource(resourceId) {
+	function removeEventResource(resourceId, reRenderView) {
 		var updatedResources = [];
+		console.log(eventResources)
 		for(var i = 0; i < eventResources.length; i++) {
 			if(eventResources[i].id != resourceId) {
 				updatedResources.push(eventResources[i]);
@@ -606,7 +607,7 @@ function Calendar(element, instanceOptions) {
 			associateResourceWithEvent(events[i]);
 		}
 		orderResources();
-		renderView(null, currentView.name, true);
+		if (reRenderView) {renderView(null, currentView.name, true);}
 	}
 
 	function clientResources(filter) {
