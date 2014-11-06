f1c37403c0e7f8d1e3ba5697cc218f1fac417606
Jonathan Wheeler <jonny@njtechnologies.co.uk>
2014-11-06 19:49:02 +0000

    remove fc-short class for agenda views

--- src/common/TimeGrid.events.js
+++ src/common/TimeGrid.events.js
@@ -65,11 +65,6 @@ $.extend(TimeGrid.prototype, {
 				seg = colSegs[i];
 				seg.el.css(this.generateSegPositionCss(seg));
 
-				// if the height is short, add a className for alternate styling
-				if (seg.bottom - seg.top < 30) {
-					seg.el.addClass('fc-short');
-				}
-
 				containerEl.append(seg.el);
 			}
 
